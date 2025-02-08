bc3ef042e0c48cc373be807d206601757b0d40fa
KubriksCodeTN <patrick.pastorelli@studenti.unitn.it>
2025-02-08 18:28:54 +0100

    clippy

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@
  * the packets sent on the network. The library exposes the trait [`Compressor`]
  * that offers a generic minimal API for compression so a user can implement their
  * own compression algorithms and easily intergate it with this library
- * 
+ *
  * By default the following compressors are available:
  * - [`HuffmanCompressor`] -> uses Huffman compression
  * - [`LZWCompressor`] -> uses LZW compression, very fast but small gains
  * - [`BypassCompressor`] -> this compressor just return the input unchanged,
  *      it is used to logically represent that no compression happens
- * 
+ *
  * # Example usage
  * ```
  * # fn main() {
@@ -39,11 +39,11 @@ pub mod lzw;
 mod test_utils;
 
 #[doc(inline)]
+pub use bypass::BypassCompressor;
+#[doc(inline)]
 pub use huffman::HuffmanCompressor;
 #[doc(inline)]
 pub use lzw::LZWCompressor;
-#[doc(inline)]
-pub use bypass::BypassCompressor;
 
 /// Offers minimal API to implement a compressor object
 pub trait Compressor {
